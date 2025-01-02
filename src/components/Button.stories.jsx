3dcebda7dfe15cdb83d89d1910f5b8bf013db861
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-02 15:48:58 -0600

    Create button and stories

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,19 @@
+import Button from './Button';
+
+const meta = {
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    label: "Click Me"
+  }
+};
+
+export const Secondary = {
+  args: {
+    label: "Get Started"
+  }
+};
