80691bd24d580c84d52e24dcd74b0994e03dd090
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-02 20:09:20 -0600

    Used HelloBackend component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
+import HelloBackend from "@/components/HelloBackend";
+import TestButton from "@/components/TestButton";
+import NavBar from "@/components/NavBar";
+
+
 export default function Home() {
-  return <h1>Coffee Shop Frontend Splash Page</h1>;
+  return (
+  <>
+    <NavBar/>
+    <h1>Coffee Shop Frontend Splash Page</h1>
+    <HelloBackend/>
+    <TestButton/>
+  </>
+  )
 }
