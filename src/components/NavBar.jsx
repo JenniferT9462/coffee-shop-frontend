2aeb0abfb19e7632715902fdb59b86bc75f7c78f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-02 19:29:27 -0600

    Fixed NavBar name

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,12 @@
 // components/Navbar.js
 import PropTypes from 'prop-types';
-export default function Navbar({title}) {
+// import '../styles/navbar.css';
+
+
+export default function NavBar({title}) {
   return <div className="navbar">Navbar Component {title}</div>;
 }
-Navbar.propTypes = {
+NavBar.propTypes = {
   // Add prop-types here
    title: PropTypes.string.isRequired,
 };
