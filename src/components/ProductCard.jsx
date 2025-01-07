9d427b281d02e682f45aef18a5a27caa948f55c7
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 17:16:08 -0600

    Updated ProductCard

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-export default function ProductCard({ title }) {
+export default function ProductCard({ product }) {
+  console.log(product)
+  if (!product) {
+    return <div>Loading...</div>; // or display an error message
+  }
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white border">
-      <div>Product Card Component {title} </div>
+    <div className="card max-w-sm rounded overflow-hidden shadow-lg bg-white border">
+      <img src={product.imageUrl} alt={product.name} />
+      <h3>{product.name}</h3>
+      <p>{product.description}</p>
+      <p>${product.price}</p>
+      <Button label="Add to Cart" />
     </div>
   );
-};
+}
 
-// Define prop types
 ProductCard.propTypes = {
-  title: PropTypes.string.isRequired, // The title of the product
-  image: PropTypes.string.isRequired, // URL for the product image
-  price: PropTypes.number.isRequired, // Price of the product
-  onAddToCart: PropTypes.func.isRequired, // Callback function when "Add to Cart" is clicked
+  product: PropTypes.object.isRequired,
 };
-
-
