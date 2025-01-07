34f47b7ba45e4e4c8f9b4082faa92f08cce65a59
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 10:30:46 -0600

    Setup index for splash page components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,23 @@
-import HelloBackend from "@/components/HelloBackend";
-import TestButton from "@/components/TestButton";
-import NavBar from "@/components/NavBar";
+// import HelloBackend from "@/components/HelloBackend";
+// import TestButton from "@/components/TestButton";
+// import NavBar from "@/components/NavBar";
+import Header from "@/components/Header";
+import HeroSection from "@/components/HeroSection";
+import Footer from "@/components/Footer";
 
 
 export default function Home() {
   return (
-  <>
-    <NavBar/>
-    <h1>Coffee Shop Frontend Splash Page</h1>
-    <HelloBackend/>
-    <TestButton/>
-  </>
+  <div className="flex flex-col min-h-screen">
+    {/* <NavBar/> */}
+    <Header/>
+    <main>
+        {/* <h1>Coffee Shop Frontend Splash Page</h1> */}
+        <HeroSection/>
+    </main>
+    <Footer title={"Brew Haven 2024"}/>
+    {/* <HelloBackend/> */}
+    {/* <TestButton/> */}
+  </div>
   )
 }
