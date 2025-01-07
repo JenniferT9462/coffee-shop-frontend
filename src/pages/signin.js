80155be3efc83e5ee2c58f0e60861f776d6a7268
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 11:48:29 -0600

    Add signin page and Header, SignUpForm and Footer components

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,7 +1,14 @@
+import Footer from "@/components/Footer";
 import SignUpForm from "@/components/SignUpForm";
+import Header from "@/components/Header";
 
 export default function signin() {
     return (
-        <SignUpForm buttonLabel="Sign In" handleClick={()=>{console.log("clicked sign in")}}/>
+        <>
+            <Header/>
+            <SignUpForm buttonLabel="Sign In" handleClick={()=>{console.log("clicked sign in")}}/>
+            <Footer title={"Brew Haven 2024"}/>
+        </>
     )
+        
 }
