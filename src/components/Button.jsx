6b4d2c0096474787a7194ce4fb0c0847939e5b3f
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 17:15:01 -0600

    Add type="button" to get the button to work

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 export default function Button({ label, handleClick }) {
   return (
-    <button onClick={handleClick} className="btn btn-primary">
+    <button type="button" onClick={handleClick} className="btn btn-primary">
       {label}
     </button>
   );
