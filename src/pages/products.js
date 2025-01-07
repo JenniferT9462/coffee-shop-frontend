1f7c00aa5600d98ff64a2baa6199ce5a52d9a232
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 17:34:58 -0600

    Add Products page to display multiple products

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,7 +1,15 @@
+import products from "../../mocks/products.json";
+import ProductCard from "@/components/ProductCard";
+
 export default function ProductsPage() {
     return (
-      <div>
-        <h1>Products Page</h1>
+      <div className="p-4">
+        <h1 className="text-3xl font-semibold text-center mb-8 text-primary">Products Page</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
       </div>
     );
   }
