cf8171085934c983edb2e1b682c8336b36735815
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 11:47:22 -0600

    Passed prop to Button component for SignUpForm

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -7,7 +7,7 @@ export default function SignUpForm({ buttonLabel }) {
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
-            <Button label="Sign Up"handleClick={()=>{console.log("clicked sign up")}}/>
+            <Button label={buttonLabel} handleClick={()=>{console.log("clicked sign up")}}/>
         </form>
     )
 }
