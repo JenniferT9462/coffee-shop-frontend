24ef56f71cb88c9219560a4f7e4208517dbce431
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 17:16:49 -0600

    Made the product page to extract id from param and import mock data

--- src/pages/product/[id].js
+++ src/pages/product/[id].js
@@ -1,7 +1,27 @@
-export default function ProductPage({ id }) {
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ProductCard from "@/components/ProductCard";
+import products from "../../../mocks/products.json";
+import { useRouter } from 'next/router';
+
+
+export default function ProductPage() {
+    const router = useRouter();
+    const { id } = router.query;
+
+    // Find the product that matches the id
+    const product = products.find((product) => product._id === id);
+
+    if (!product) {
+      return <div>Product not found</div>;
+    }
+
     return (
       <div>
-        <h1>Product Page for product &num; { id }</h1>
+        <Header/>
+        <h1>Product Page for product &#35; { id }</h1>
+        <ProductCard product={product} />
+        <Footer title={"Brew Haven 2024"}/>
       </div>
     );
   }
