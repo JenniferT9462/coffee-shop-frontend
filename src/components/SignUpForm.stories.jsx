d77402b62f5f7ea70f686a78a7c827147c2919a3
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 10:53:34 -0600

    Verify form component in storybook

---
+++ src/components/SignUpForm.stories.jsx
@@ -0,0 +1,13 @@
+import SignUpForm from './SignUpForm';
+
+const meta = {
+  component: SignUpForm,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    buttonLabel: "Sign Up"
+  }
+};
