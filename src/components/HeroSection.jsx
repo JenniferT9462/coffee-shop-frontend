c7424e15a7a82480c5a2d7435eb12822d138dc2d
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-07 22:44:01 -0600

    Changed to a Daisyui Hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,34 @@
 // components/HeroSection.jsx
-import Button from './Button';
-import { useRouter } from 'next/router';
+import Button from "./Button";
+import { useRouter } from "next/router";
 
 const HeroSection = () => {
-    const router = useRouter();
-    function handleCtaClick() {
-        console.log('CTA button clicked!');
-        router.push('/signup');
-    }
-    return (
-        <section className="text-center text-base-100 py-20 bg-[url('/splashImage.jpeg')] bg-cover h-screen bg-center">
-        <h2 className="text-4xl font-bold mb-4">Freshly Brewed Happiness</h2>
-        <p className="text-lg mb-6">Indulge in the finest coffee crafted just for you.</p>
-        <Button label="Sign Up Now" handleClick={handleCtaClick} />
-        </section>
-    );
-    };
+  const router = useRouter();
+  function handleCtaClick() {
+    console.log("CTA button clicked!");
+    router.push("/signup");
+  }
+  return (
+    <div className="hero min-h-screen bg-base-100"  style={{
+        backgroundImage: "url('/splashImage.jpeg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+        {/* Overlay */}
+        <div className="hero-overlay bg-opacity-60 bg-black"></div>
+
+        <div className="hero-content text-center text-neutral-content">
+            <div className="max-w-md">
+                <h2 className="text-5xl font-bold">Freshly Brewed Happiness</h2>
+                <p className="text-lg py-6">
+                    Indulge in the finest coffee crafted just for you.
+                </p>
+                <Button label="Sign Up Now" handleClick={handleCtaClick} />
+            </div>
+        </div>
+    </div>
+  );
+};
 
 export default HeroSection;
