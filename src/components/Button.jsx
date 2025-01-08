ccf975b486a3faa0788ce77bb15a94e387f5e025
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-08 17:23:21 -0600

    Fixed formatting

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 export default function Button({ label, handleClick }) {
   return (
-    <button type="button" onClick={handleClick} className="btn btn-primary w-full">
+    <button 
+      type="button" 
+      onClick={handleClick} 
+      className="btn btn-primary w-full">
       {label}
     </button>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,16 +3,21 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 export default function ProductCard({ product }) {
-  console.log(product)
+  console.log(product);
   if (!product) {
-    return <div>Loading...</div>; // or display an error message
+    return <div>Loading...</div>; 
   }
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
       <div className="card w-64 bg-base-100 shadow-xl">
+        {/* Daisy UI Figure */}
         <figure className="h-48">
-          <img src={product.imageUrl} alt={product.name} className="object-cover w-full h-full" />
+          <img
+            src={product.imageUrl}
+            alt={product.name}
+            className="object-cover w-full h-full"
+          />
         </figure>
         <div>
           <h3 className="card-title">{product.name}</h3>
@@ -20,7 +25,10 @@ export default function ProductCard({ product }) {
           <p>${product.price}</p>
         </div>
         <div className="mt-auto card-actions justify-end">
-          <Button label="Add to Cart" handleClick={() => alert("Product Added to Cart!")} />
+          <Button
+            label="Add to Cart"
+            handleClick={() => alert("Product Added to Cart!")}
+          />
         </div>
       </div>
     </div>
