d0865f14f3628ad5b9578fe25e8bfc7fdca02455
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-08 22:43:37 -0600

    Add prop drilling to signup page

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,17 +1,21 @@
 import SignUpForm from "@/components/SignUpForm";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useRouter } from "next/router";
 
 export default function signup() {
+  const router = useRouter();
+  function signUp() {
+    alert('You are Signed Up!!');
+    router.push('/signin');
+  }
   return (
     <>
       <Header />
       <SignUpForm
         title="Sign Up Now!"
         buttonLabel="Sign Up"
-        handleClick={() => {
-          console.log("clicked sign up");
-        }}
+        handleSignUp={signUp}
       />
       <Footer title={"Brew Haven 2024"} />
     </>
