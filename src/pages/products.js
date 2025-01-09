ecd290e9b03ca0bed63e992c90b8674529140c6d
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-08 22:41:40 -0600

    Add Footer and Header to ProductsPage

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,15 +1,21 @@
 import products from "../../mocks/products.json";
 import ProductCard from "@/components/ProductCard";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function ProductsPage() {
     return (
-      <div className="p-4">
-        <h1 className="text-3xl font-semibold text-center mb-8 text-primary">Products Page</h1>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
+      <div>
+        <Header/>
+        <div className="p-4">
+          <h1 className="text-3xl font-semibold text-center mb-8 text-primary">Products Page</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+          </div>
         </div>
+        <Footer/>
       </div>
     );
   }
