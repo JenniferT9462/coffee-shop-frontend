a112002b3f73f779796b53e95c746d825196f936
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-09 20:30:34 -0600

    Add function to include name of product

--- src/pages/products.js
+++ src/pages/products.js
@@ -4,18 +4,32 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function ProductsPage() {
+  const allProducts = products.map((product) => {
+    function addToCart() {
+      alert(product.name + " Has Been Added to Your Cart!!!");
+      // TODO: Add fetch to backend
+    }
     return (
-      <div>
-        <Header/>
-        <div className="p-4">
-          <h1 className="text-3xl font-semibold text-center mb-8 text-primary">Products Page</h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-            {products.map((product) => (
-              <ProductCard key={product.id} product={product} />
-            ))}
-          </div>
-        </div>
-        <Footer title="Brew Haven"/>
-      </div>
+      <ProductCard
+        key={product._id}
+        product={product}
+        onAddToCart={addToCart}
+      />
     );
-  }
+  });
+
+  return (
+    <div>
+      <Header />
+      <div className="p-4">
+        <h1 className="text-3xl font-semibold text-center mb-8 text-primary">
+          Products Page
+        </h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {allProducts}
+        </div>
+      </div>
+      <Footer title="Brew Haven" />
+    </div>
+  );
+}
