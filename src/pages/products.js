3526f46ff9cd41e6007de45b5972751be09a8746
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-10 13:36:13 -0600

    Add router function to direct to the [id].js page

--- src/pages/products.js
+++ src/pages/products.js
@@ -2,18 +2,28 @@ import products from "../../mocks/products.json";
 import ProductCard from "@/components/ProductCard";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useRouter } from "next/router";
 
 export default function ProductsPage() {
+  const router = useRouter();
+
   const allProducts = products.map((product) => {
     function addToCart() {
       alert(product.name + " Has Been Added to Your Cart!!!");
       // TODO: Add fetch to backend
     }
+
+    // Redirect to product/[id].js with template literal to pass a js variable
+    function viewProduct() {
+      router.push(`/product/${product._id}`);
+    }
+
     return (
       <ProductCard
         key={product._id}
         product={product}
         onAddToCart={addToCart}
+        onViewProduct={viewProduct}
       />
     );
   });
