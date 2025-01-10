4b4762eb5ab296c3ce083199b893b18669990c31
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-09 20:31:19 -0600

    Add prop drilling to signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -4,10 +4,15 @@ import Header from "@/components/Header";
 import SignInForm from "@/components/SignInForm";
 
 export default function signin() {
+  function signIn() {
+    alert("Your are Signed In!!!")
+    // TODO: add redirect?
+  }
+
   return (
     <>
       <Header />
-      <SignInForm buttonLabel="Sign In" title="Sign In Now!" handleSignIn={() => alert("You are Signed In!")}/>
+      <SignInForm buttonLabel="Sign In" title="Sign In Now!" handleSignIn={signIn}/>
       <Footer title={"Brew Haven"} />
     </>
   );
