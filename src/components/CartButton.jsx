86c1a9e8b9f4e098cebfb6b8d3270c99728587b6
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-10 13:54:32 -0600

    Removed bg for CartButton

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 export default function CartButton({ itemCount }) {
   return (
-    <button className="bg-primary relative">
+    <button className="relative">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width={34}
