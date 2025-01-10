94ebea93755e466df1908bf5c4fecedaa7be2cd0
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-10 16:41:35 -0600

    Add loader to [id].js

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,9 @@
 // import PropTypes from 'prop-types';
 export default function Loader() {
     return (
-        // <div className="flex items-center justify-center h-screen bg-gray-100">
-        //   <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin"></div>
-        // </div>
-        <div>Loader Component</div>
+        <div>
+          <span className="loading loading-spinner text-accent">Loading...</span>
+        </div>
+        
       );
 }
