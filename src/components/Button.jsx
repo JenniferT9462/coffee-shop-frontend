556c4ff4c0c471246431e4568ff735f74911558a
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-09 23:53:14 -0600

    Fixing button classes for variant prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,18 @@
-import PropTypes from 'prop-types';
-export default function Button({ label, handleClick, variant="primary" }) {
+import PropTypes from "prop-types";
+export default function Button({ label, handleClick, variant = "primary" }) {
+  const variantClass =
+    {
+      primary: "btn-primary",
+      secondary: "btn-secondary",
+      error: "btn-error",
+    }[variant] || "btn-primary"; // Fallback to "btn-primary" if no match
+
   return (
-    <button 
-      type="button" 
-      onClick={handleClick} 
-      className={`btn btn-${variant}`}>
+    <button
+      type="button"
+      onClick={handleClick}
+      className={`btn ${variantClass}`}
+    >
       {label}
     </button>
   );
