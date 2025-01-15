65b4fbe4b2a2ba38cfdfb46a46356ee888f02e95
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-14 20:45:53 -0600

    Trying out daisyUI theme controller

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 import "@/styles/navbar.css";
 import { Lobster, Lora } from 'next/font/google';
 
+
+
 const lobster = Lobster({ weight: '400', subsets: ['latin'] });
 const lora = Lora({ weight: ['400', '700'], subsets: ['latin'] });
 
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <div data-theme="coffeeShop" className={`${lobster.className} ${lora.className}`}>
       <Component {...pageProps} />
+      
     </div>
   ) 
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -13,6 +13,7 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+        
       </body>
     </Html>
   );
