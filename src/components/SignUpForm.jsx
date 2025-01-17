7487d09e68ab1f0d5f0038974412f46888586281
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-16 20:53:15 -0600

    Add regex function but haven't use it

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -33,6 +33,16 @@ export default function SignUpForm({ buttonLabel, title, handleSignUp }) {
   const [emailValue, setEmailValue] = useState("");
   const [emailIsValid, setEmailIsValid] = useState(false);
 
+  // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  // // Example usage:
+  // const email = "example@example.com";
+  // if (emailRegex.test(email)) {
+  //   console.log("Valid email address.");
+  // } else {
+  //   console.log("Invalid email address.");
+  // }
+  
   function onEmailChange(event) {
     const newEmailValue = event.target.value;
     if (newEmailValue.includes("@")) {
