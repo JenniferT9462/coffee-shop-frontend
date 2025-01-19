ed1fafbbdf411c75ad9fb7ba8b578331e9cf2dae
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-19 12:14:53 -0600

    Set up React Query provider in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,22 @@
 import "@/styles/globals.css";
 import "@/styles/navbar.css";
-import { Lobster, Lora } from 'next/font/google';
+import { Lobster, Lora } from "next/font/google";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const lobster = Lobster({ weight: '400', subsets: ['latin'] });
-const lora = Lora({ weight: ['400', '700'], subsets: ['latin'] });
+const lobster = Lobster({ weight: "400", subsets: ["latin"] });
+const lora = Lora({ weight: ["400", "700"], subsets: ["latin"] });
 
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <div data-theme="coffeeShop" className={`${lobster.className} ${lora.className}`}>
-      <Component {...pageProps} />
-      
-    </div>
-  ) 
+    <QueryClientProvider client={queryClient}>
+      <div
+        data-theme="coffeeShop"
+        className={`${lobster.className} ${lora.className}`}
+      >
+        <Component {...pageProps} />
+      </div>
+    </QueryClientProvider>
+  );
 }
