f1e4e92a9eb1d7e1dd9f72a91f45dfdf40af0661
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-27 17:54:03 -0600

    Preload fonts in _document head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,17 +3,32 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-       <Head>
-          {/* Import Lobster and Lora fonts from Google Fonts */}
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Lobster&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+      <Head>
+        <link
+          rel="preload"
+          href="/_next/static/media/6e48331146f8b4ca-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+          data-next-font="size-adjust"
+        />
+        <link
+          rel="preload"
+          href="/_next/static/media/3a8dc5763a8ae4b2-s.p.woff2"
+          as="font"
+          type="font/woff2"
+          crossorigin="anonymous"
+          data-next-font="size-adjust"
+        />
+        {/* Import Lobster and Lora fonts from Google Fonts */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lora:wght@400;700&family=Lobster&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
-        
       </body>
     </Html>
   );
