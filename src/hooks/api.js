fbe2841d1a04b934ba6a3bd2594437499b40d765
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-01-29 10:45:43 -0600

    Uncommented useFetch for products page not to be protected

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,35 +1,35 @@
 import { useState, useEffect } from "react";
 
-// export function useFetch(url, initialState) {
-//     const [data, setData] = useState(initialState);
-//     const [fetchError, setFetchError] = useState(false);
-//     const [loading, setLoading] = useState(true);
+export function useFetch(url, initialState) {
+    const [data, setData] = useState(initialState);
+    const [fetchError, setFetchError] = useState(false);
+    const [loading, setLoading] = useState(true);
 
-//     useEffect(() => {
-//         async function fetchData() {
-//             try {
-//               setLoading(true);
-//               const result = await fetch(url);
-//               if (!result.ok) {
-//                 console.log("fetch failed with " + result.status);
-//                 setFetchError(true);
-//               } else {
-//                 const productData = await result.json();
-//                 setData(productData.products);
-//                 console.log("Fetched Products:", productData); // Debug log
-//               }
-//             } catch (error) {
-//               console.error("Error fetching products:", error);
-//               setFetchError(true);
-//             } finally {
-//               setLoading();
-//             }
-//         }
-//         fetchData()
-//     }, [url]);
-//     return [fetchError, loading, data];
+    useEffect(() => {
+        async function fetchData() {
+            try {
+              setLoading(true);
+              const result = await fetch(url);
+              if (!result.ok) {
+                console.log("fetch failed with " + result.status);
+                setFetchError(true);
+              } else {
+                const productData = await result.json();
+                setData(productData.products);
+                console.log("Fetched Products:", productData); // Debug log
+              }
+            } catch (error) {
+              console.error("Error fetching products:", error);
+              setFetchError(true);
+            } finally {
+              setLoading();
+            }
+        }
+        fetchData()
+    }, [url]);
+    return [fetchError, loading, data];
    
-// }
+}
 
 export function useAuthFetch(url, initialState, token) {
   const [data, setData] = useState(initialState);
