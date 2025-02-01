ef8d6f424c47be6aa109ac95c62d206a0b33ad51
Jennifer L Tarleton <63979357+JenniferT9462@users.noreply.github.com>
2025-02-01 16:49:38 -0600

    Fixed auth provider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,25 +1,22 @@
 import "@/styles/globals.css";
 import "@/styles/navbar.css";
 import { Lobster, Lora } from "next/font/google";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "@/context/AuthContext";
 const lobster = Lobster({ weight: "400", subsets: ["latin"] });
 const lora = Lora({ weight: ["400", "700"], subsets: ["latin"] });
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }) {
   return (
-    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
       <div
         data-theme="coffeeShop"
         className={`${lobster.className} ${lora.className}`}
       >
-        <AuthProvider>
-          <Component {...pageProps} />
-        </AuthProvider>
-        
+        <Component {...pageProps} />
       </div>
-    </QueryClientProvider>
+    </AuthProvider>
   );
 }
